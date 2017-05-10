b4c2cc25625e1509df60b5db04c2a877529070c8
shuzhenguo <shuzhenguovip@163.com>
2017-05-10 19:46:35 +0800

    隐藏的文字点击展开
    隐藏的文字点击展开

--- lableDemo/ViewController.m
+++ lableDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "ExpandLabel.h"
 
 @interface ViewController ()
+{
+    ExpandLabel * expandLab;
+    UILabel * bottomLab;
+}
 
 @end
 
@@ -17,7 +22,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initUI];
+
 }
+
+- (void)initUI {
+    
+    
+    __block typeof(self) weakSelf = self;
+    
+    expandLab = [[ExpandLabel alloc] initWithFrame:CGRectMake(0, 20, 320, 0)];
+    [expandLab setLabelText:@"放大发发看房看房卡难看啦女阿卡拉萨路那边那把大把大神了奥德赛吧是不是把那边艾斯比啊是\n\ndafa放大发发看房看房卡难看啦女阿卡拉萨路那边那把大把大神了奥德赛吧是不是把那边艾斯比啊是\n\ndafa放大发发看房看房卡难看啦女阿卡拉萨路那边那把大把大神了奥德赛吧是不是把那边艾斯比啊是\n\ndafa放大发发看房看房卡难看啦女阿卡拉萨路那边那把大把大神了奥德赛吧是不是把那边艾斯比啊是\n\ndafavbdfjbvdjfvbdvbefeugfuegfuyergfuyergyhchdsvhvhgvhcvchvdhvfefyewuyfefgewfyfy" expandBlock:^{
+        
+        
+        //点击展开 界面重新布局
+        [weakSelf updateUI];
+    }];
+    expandLab.backgroundColor=[UIColor redColor];
+    
+    [self.view addSubview:expandLab];
+    
+}
+
+- (void)updateUI {
+    
+    NSLog(@"%f",expandLab.frame.size.height);
+
+//    bottomLab.frame = CGRectMake(0, expandLab.frame.origin.y + expandLab.frame.size.height + 20, 320, 20);
+}
+
 
 
 - (void)didReceiveMemoryWarning {
